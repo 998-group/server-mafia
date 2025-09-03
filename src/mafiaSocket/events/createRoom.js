7814e163e050d283714008007deda28aa67f4b6c
madalievsardor <madalievsardor33@gmail.com>
2025-09-03 23:56:34 +0500

    READY GAME

--- src/mafiaSocket/events/createRoom.js
+++ src/mafiaSocket/events/createRoom.js
@@ -38,18 +38,68 @@ export const createRoom = async (io, socket, data) => {
   console.log("NEW ROOM: ", newRoom);
 };
 
-export const joinRoom = async (io, socket, data) => {};
+export const joinRoom = async (io, socket, data) => {
+  const room = await Game.findOne({ roomId: data.roomId });
+  if (!room) {
+    socket.emit('error', { message: 'Room not found' });
+    return
+  }
+  console.log("data",data)
+  const alreadyJoined = room.players.find(p => String(p.userId) === data.userId)
+  if (!alreadyJoined) {
+    room.players.push({ userId: data.userId, username: data.username, isAlive: true });
+    await room.save()
+  }
+  socket.join(data.roomId);
 
-export const leaveRoom = async (io, socket, data) => {};
+  socket.emit("joined_room", room);
+  io.to(data.roomId).emit("update_players", room.players)
+};
+
+
+export const leaveRoom = async (io, socket, data) => {
+  const room = await Game.findOne({ roomId: data.roomId });
+  if (!room) {
+    socket.emit("error", { message: "Room not found" });
+    return;
+  }
+
+  room.players = room.players.filter(
+    (p) => String(p.userId) !== String(data.userId)
+  );
+
+  if (room.players.length === 0) {
+    await Game.deleteOne({ roomId: data.roomId });
+    console.log(`❌ Room deleted: ${room.roomName}`);
+  } else {
+    await room.save();
+    io.to(data.roomId).emit("update_players", room.players);
+  }
+
+  io.emit(
+    "update_rooms",
+    await Game.find({ players: { $not: { $size: 0 } } })
+      .sort({ createdAt: -1 })
+      .limit(100)
+  );
+
+  socket.leave(data.roomId);
+};
 
 export const readyGame = async (io, socket, data) => {
   console.log("READY GAME: ", data);
   const room = await Game.findOne({ roomId: data.roomId });
-  const player = await room.players.find((p) => String(p.userId) === data.userId);
-  console.log("ROOM     : ", room.players);
-  console.log("PLAYer   : ", player);
+  const player = room.players.find((p) => String(p.userId) === data.userId);
+  console.log("ROOM: ", room.players);
+  console.log("PLAYer: ", player);
   player.isReady = player.isReady ? false : true;
   await room.save();
 
   io.to(data.roomId).emit("update_players", room.players);
+
+  const allReady = room.players.length >= 2 && room.players.every(p => p.isReady) 
+  if(allReady) {
+    io.to(data.roomId).emit("start_game")
+  }
+
 };
--- src/mafiaSocket/socketGame.js
+++ src/mafiaSocket/socketGame.js
@@ -1,12 +1,19 @@
-import { createRoom, readyGame } from "../mafiaSocket/events/createRoom.js";
+import { createRoom, joinRoom, leaveRoom, readyGame } from "../mafiaSocket/events/createRoom.js";
+import Game from "../models/Game.js";
 
 export const socketGame = (io) => {
     console.log(`🎮 Game Socket Handler initialized`);
 
-    io.on("connection", (socket) => {
+    io.on("connection", async (socket) => {
         console.log(`🔌 User connected: ${socket.id}`);
-
+        const rooms = await Game.find({ players: { $not: { $size: 0 } } })
+            .sort({ createdAt: -1 })
+            .limit(100);
+        socket.emit("update_rooms", rooms);
+        
+        socket.on("leave_room", (data) => leaveRoom(io, socket, data));
         socket.on("create_room", (data) => createRoom(io, socket, data));
+        socket.on("join_room", (data) => joinRoom(io, socket, data));
         socket.on("ready", (data) => readyGame(io, socket, data));
-    })    
+    })
 }
