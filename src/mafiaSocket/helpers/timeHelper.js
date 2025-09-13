4382edabb97b4d2541470c03d140f8c4d7ff043c
Bekzodmirzaaliyev <bekzodmirzaaliyev27@gmail.com>
2025-09-13 15:44:55 +0500

    ASD

--- src/mafiaSocket/helpers/timeHelper.js
+++ src/mafiaSocket/helpers/timeHelper.js
@@ -3,7 +3,7 @@ import Game from "../../models/Game.js";
 export const timerEnd = async (io, data, socket) => {
   const findGame = await Game.findOne({ roomId: data.roomId });
   if (!findGame) return socket.emit("error", { message: "Game not found" });
-  console.log("TIMER_END: ",data)
+  console.log("TIMER_END: ", data);
   const mafiaCount = findGame.players.filter((p) => p.gameRole === "mafia");
   const detectiveCount = findGame.players.filter(
     (p) => p.gameRole === "detective"
@@ -36,10 +36,10 @@ export const timerEnd = async (io, data, socket) => {
     } else if (villager.length <= 1 && mafiaCount.length >= 1) {
       findGame.phase = "ended";
       await findGame.save();
+    } else {
+      findGame.phase = "day";
+      await findGame.save();
     }
-
-    findGame.phase = "day";
-    await findGame.save();
   } else if (findGame.phase === "day") {
     if (villager.length >= 2 && mafiaCount.length == 0) {
       console.log("YUTDI:", villager);
@@ -48,12 +48,12 @@ export const timerEnd = async (io, data, socket) => {
     } else if (villager.length <= 1 && mafiaCount.length >= 1) {
       findGame.phase = "ended";
       await findGame.save();
+    } else {
+      findGame.phase = "night";
+      await findGame.save();
     }
-
-    findGame.phase = "night";
-    await findGame.save();
   }
-  console.log("FINDED_GAME: ", findGame)
+  console.log("FINDED_GAME: ", findGame);
   io.to(data.roomId).emit("update_phase", findGame.phase);
 
   console.log("SARDOR BICH BOLA: ", findGame);
